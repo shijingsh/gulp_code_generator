47fa9c6de221f003a679a6a773b255a275a3e24b
liukefu2050 <liukefu2050@sina.com>
2021-05-13 16:05:24 +0800

    1

--- demoPackage/service/DemoService.java
+++ demoPackage/service/DemoService.java
@@ -8,4 +8,6 @@ import com.mg.util.SearchVo;
 public interface DemoService extends BaseService<DemoEntity>{
 
     PageTableVO findPageList(SearchVo searchVo);
+
+    DemoEntity saveDemo(DemoEntity entity);
 }
